f6079da1242da3fcbb6b9dafebaea1e9f69e282b
mdabidfaisal <abid.nwu.cse@gmail.com>
2025-11-29 04:56:49 +0600

    fix allowhost

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Dev server
+  server: {
+    host: true,          // allow external access
+    strictPort: false,   // allow fallback ports if needed
+  },
+
+  // Preview server (used in Docker)
+  preview: {
+    host: true,          // allow all hosts
+    port: 1007,
+    allowedHosts: ['*'], // allow * ANY HOST *
+  },
 })
