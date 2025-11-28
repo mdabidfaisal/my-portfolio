cce76bfb681f2d7779e6b3115190d7322b09286d
mdabidfaisal <abid.nwu.cse@gmail.com>
2025-11-29 04:59:39 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // Dev server
-  server: {
-    host: true,          // allow external access
-    strictPort: false,   // allow fallback ports if needed
-  },
-
-  // Preview server (used in Docker)
   preview: {
-    host: true,          // allow all hosts
+    host: true,
+    allowedHosts: ['afaisalfolio.wyzeek.com'],
     port: 1007,
-    allowedHosts: ['*'], // allow * ANY HOST *
   },
+  server: {
+    host: true
+  }
 })
